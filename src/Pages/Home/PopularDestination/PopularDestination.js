860dd2c9891b052d1d4e024c6f65db9942e552f3
shoebbint <shoebbint@gmail.com>
2023-02-21 14:53:17 +0600

    loading-added

--- src/Pages/Home/PopularDestination/PopularDestination.js
+++ src/Pages/Home/PopularDestination/PopularDestination.js
@@ -12,7 +12,7 @@ const PopularDestination = () => {
                 <div class="row">
                     <div class="col-md-12">
                         <div class="d-flex justify-content-between align-items-center breaking-news bg-white">
-                            <div class="d-flex flex-row flex-grow-1 flex-fill justify-content-center bg-danger py-2 text-white px-1 news"><span class="d-flex align-items-center">&nbsp;CNN News</span></div>
+                            <div class=""></div>
                             <marquee class="news-scroll" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();"> <a href="#">
                                 <h1>Popular Destinations
                                     Favorite Places to Visit</h1>
